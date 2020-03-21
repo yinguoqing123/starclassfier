695d7d6d649cc61654ea67679432511f57cb57f6
yinguoqing <yinguoqing@shein.com>
2020-03-21 12:07:11 +0800

    修正一些代码错误

--- train_deep.py
+++ train_deep.py
@@ -10,15 +10,18 @@ import keras.backend as K
 from sklearn.model_selection import train_test_split
 import json
 import gc
-
+from keras.losses import  categorical_crossentropy
 
 """
 初始分数：0.9816
 1、流量归一化  0.9832
 2、数据增强，改变batch内各个类的比例 0.9819
 3、更改网络结构，去除两个滤波器相减操作 0.9812
-4、更改网络结构，增加差分通道 
+4、更改网络结构，增加差分通道 0.9826 
 4、加入差分特征
+
+relu+softmax_entrypy： loss出现nan值，
+原因：数据中出现了nan值
 """
 
 np.random.seed(2020)
@@ -30,6 +33,7 @@ def score_loss(y_true, y_pred):
         y_pred_ = K.constant([list(i)]) * y_pred
         loss += 0.5 * K.sum(y_true_ * y_pred_) / K.sum(y_true_ + y_pred_ + K.epsilon())
     return - K.log(loss + K.epsilon())
+
 
 # 数据读取。
 # 光谱的存储方式为一个txt存一个样本，txt里边是字符串格式的序列
@@ -102,7 +106,52 @@ def add_diff_channel(data, features):
 
 train_data = add_diff_channel(train_data, features)
 
-train_data, valid_data = train_test_split(train_data, test_size=0.2, stratify=train_data.label)
+# 这些样本2600维全为1， 出现nan值
+id_list = ['01703e6377847e1232e466ac18c3b77f', '03a7a75a6d27b27a2a4beeb1ef9f83a9',
+       '0cd6b854b430974a9cff3caf1fa780ca', '0e44f3c097a1a12f614f4bdb4b10bdf1',
+       '1082efc11732329f933752a810f9638e', '15e50a93629de58527474647f7577953',
+       '1605d57fe264730b6b7bc0302e851907', '17b3a1e3dbb215d8129e22c9251aafb9',
+       '1875e0da165004aaf9a3843a2c605644', '1a84416eb6f05016513f3a6df412c6d5',
+       '1c29b2fc6b9c2a955d371e07aa4b21ba', '1e364be5461ee97296e30cc5f675d865',
+       '221e67a9b1a09ab5faff9fe20244672e', '2a565745a52fbc5a36d658ee6d3ef4ca',
+       '2e6c37e4aeb15e3ef71f86db0f854bbd', '2e8eacab4f4ee1fe286c8cdfb2d0b9e2',
+       '2fbc59a095a43578c3c48b6f10a32fa9', '320396000f93c06ad6252022f5c823ed',
+       '35cd808100d9627a457d694aca45cb9b', '3ba9295fcdb66dec02b926c76728f586',
+       '3e297f5727bc1b0f927781c6f26796cf', '3ff32f148d36b8c7b7716cae70e458a4',
+       '40b4475ef67ccb6be495ba7e7306a73c', '432e9277f02cb50cbfead4a598690368',
+       '46d91b93560df1e530bb1f23aaa186e8', '474f687e9fe98b8dc5c6503b45839228',
+       '47a70662ac5bcd29d13b0231f9f7104e', '4ed98d8e31794369688ba2b3292def0f',
+       '58811ed9f45371624bd55c0e8af0cc03', '606ec43bcf40625164aec62543afcf18',
+       '61e0ac120022d19e810e524a20f5d59a', '635a597ce0400a43000bfc2eaaf50509',
+       '636588680f95da1219d81fc3147c4084', '64541a18375c93d05dc2144fa089a730',
+       '6614e1477c5c27139521e16612a8b96c', '686e70684ea2681ec013aecd74dc0ae0',
+       '6b963246340127b13783ac1ea4b8e910', '6e7962cac7b39cbb0d0bb547a641269f',
+       '6ffcba65a50c7e3d69810ae0bb412c50', '767805f72365523192916f1ba6bb45b4',
+       '7890916cc420a5a6444dba66bca3f045', '789c28ef83bf521ce797b27cc8ad85e9',
+       '80f3fe532ed0ba20436386668444b2c6', '81725be21308ff789a2e5e1b59884260',
+       '81e1178a6c78c24cb3d1e802cab7c32f', '890e03cc4fea98f90f3a0ce93c3c03f1',
+       '8ad6a7cdba22246da289c33a3b8d0c15', '8da1301aaa26e95e77821d6868f1dca3',
+       '8e36e95638b867ff9ed600302feb6904', '917d75ba8bcddfbabc12b11dca6041e8',
+       '94f0aaca98802d98665e5923aa79f710', '96369a0d73b5f9cc4f42a774597eb65c',
+       '97151a86109c97d16baacd12d6b248df', '986020efe3b290bea51d9521f5997eb6',
+       '998ebc3e154b577e345099c71e939db6', '9bb3241fb93b387f2f5ddce36298e824',
+       '9cddd8c1b56052fa1e29fb0985d2901c', 'a13ac2947b713bc6c8f41db03c30e94f',
+       'a4b17db8c84b5bfe613bcacde8428103', 'a680a2b562a1616106292c181954da6d',
+       'a817d7151550beeb27582a95624d9b67', 'add268adc1d753045051b764f5ae4360',
+       'b1c5b691e65542339f89c97f579ae08a', 'b241f3185aa227dc285f10e907faf915',
+       'b4a6ba6d51fb126398cad8b932a8ac46', 'b8a536e26b61547587d1b65034cf7ba3',
+       'bd94f2755576405cd5e683cf84aea497', 'c24cbc0bef278cb8b4b9413597dc2783',
+       'c29723e9a391cf30dbdfa3472875fba7', 'c3c6f01ec02a569ee1a52dd10b48adc3',
+       'cb7d269674a2752a27ef6b6cff5b6a47', 'ce928dbbbc6353fc0dcc71b1d829e43d',
+       'ce9cd15916aa40ff702c1af344d95ded', 'cff2601b4dc16aa883f723a482a027b7',
+       'db091771789021300452c8fa3e721a7f', 'dda336d608e764cc717136f5f34c0957',
+       'e318fb7933c001aaca2d68f5caa13dc8', 'e4540e35cee849e7d25595c7e5d86ebe',
+       'ea18f3abab3bb6a717693ab1e3391363', 'ea81609d6802f7097893d6be81548cc0',
+       'ee8957893397c0a4f580457ebab68d4d', 'f87de7c8882124b456bac216b2b46b29',
+       'ffc69e40a69f8aeba3f761329cdc3e6a']
+
+train_data = train_data[~train_data.index.isin(id_list)]
+train_data, valid_data = train_test_split(train_data, test_size=0.2, stratify=train_data.label, random_state=2020)
 
 """ 
 # 数据增强
@@ -247,14 +296,14 @@ def get_bad_case(valid_data):
     for i in range(steps):
         if i == steps - 1:
             X = np.concatenate([np.expand_dims(valid_data.iloc[cur:][features].values, 2),
-                                np.expand_dims(valid_data[cur:][features_diff].values, 2)], 2)
+                                np.expand_dims(valid_data.iloc[cur:][features_diff].values, 2)], 2)
             y = model.predict(X)
             proba.extend(y)
             y = y.argmax(axis=1)
             Y.extend(list(y))
         else:
             X = np.concatenate([np.expand_dims(valid_data.iloc[cur:cur+batch_size][features].values, 2),
-                                np.expand_dims(valid_data[cur:cur+batch_size][features_diff].values, 2)], 2)
+                                np.expand_dims(valid_data.iloc[cur:cur+batch_size][features_diff].values, 2)], 2)
             y = model.predict(X)
             proba.extend(y)
             y = y.argmax(axis=1)
